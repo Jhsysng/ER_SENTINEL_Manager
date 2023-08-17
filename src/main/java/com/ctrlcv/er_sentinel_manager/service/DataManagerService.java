86e8742c87547071f0ec8c4dd54366a585d7119c
Jhsysng <screenshot161@gmail.com>
2023-08-17 23:46:09 +0900

    feat LogManager DataManager

--- src/main/java/com/ctrlcv/er_sentinel_manager/service/DataManagerService.java
+++ src/main/java/com/ctrlcv/er_sentinel_manager/service/DataManagerService.java
@@ -1,21 +1,17 @@
 package com.ctrlcv.er_sentinel_manager.service;
 
-import com.ctrlcv.er_sentinel_manager.data.dto.ERoomResponseDto;
-import com.ctrlcv.er_sentinel_manager.data.dto.ESevereResponseDto;
-import com.ctrlcv.er_sentinel_manager.data.dto.EquipmentResponseDto;
-import com.ctrlcv.er_sentinel_manager.data.dto.HospitalResponseDto;
-import com.ctrlcv.er_sentinel_manager.data.entity.EmergencyRoom;
-import com.ctrlcv.er_sentinel_manager.data.entity.EmergencyRoomSevereCapacityInfo;
-import com.ctrlcv.er_sentinel_manager.data.entity.Hospital;
-import com.ctrlcv.er_sentinel_manager.data.entity.HospitalEquipment;
+import com.ctrlcv.er_sentinel_manager.data.dto.*;
+import com.ctrlcv.er_sentinel_manager.data.entity.*;
 import com.ctrlcv.er_sentinel_manager.data.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -96,12 +92,62 @@ public class DataManagerService {
 
     public ESevereResponseDto getEmergencySevere(String dutyId){
         log.info("[getEmergencySevere] input dutyId:{}",dutyId);
-        Optional<EmergencyRoomSevereCapacityInfo> emergencyRoomSevereCapacityInfo=emergencyRoomSevereCapacityInfoRepository.findByHospitalDutyId(dutyId);
+        Optional<EmergencyRoomSevereCapacityInfo> emergencyRoomSevereCapacityInfo=emergencyRoomSevereCapacityInfoRepository.findByEmergencyRoomSevereCapacityInfodutyId(dutyId);
+        if(emergencyRoomSevereCapacityInfo.isPresent()){
+            log.info("[getEmergencySevere] emergencyRoomSevereCapacityInfo Service do");
+            ESevereResponseDto eSevereResponseDto=new ESevereResponseDto();
+            eSevereResponseDto.setDutyId(emergencyRoomSevereCapacityInfo.get().getHospital().getDutyId());
+            eSevereResponseDto.setMyocardialInfarction(emergencyRoomSevereCapacityInfo.get().getMyocardialInfarction());
+            eSevereResponseDto.setCerebralInfarction(emergencyRoomSevereCapacityInfo.get().getCerebralInfarction());
+            eSevereResponseDto.setSubarachnoidHemorrhage(emergencyRoomSevereCapacityInfo.get().getSubarachnoidHemorrhage());
+            eSevereResponseDto.setOtherBrainHemorrhage(emergencyRoomSevereCapacityInfo.get().getOtherBrainHemorrhage());
+            eSevereResponseDto.setThoracicAorta(emergencyRoomSevereCapacityInfo.get().getThoracicAorta());
+            eSevereResponseDto.setAbdominalAorta(emergencyRoomSevereCapacityInfo.get().getAbdominalAorta());
+            eSevereResponseDto.setGallbladderDisease(emergencyRoomSevereCapacityInfo.get().getGallbladderDisease());
+            eSevereResponseDto.setBileDuctDisease(emergencyRoomSevereCapacityInfo.get().getBileDuctDisease());
+            eSevereResponseDto.setNonTraumaticAbdominalEmergency(emergencyRoomSevereCapacityInfo.get().getNonTraumaticAbdominalEmergency());
+            eSevereResponseDto.setInfantIntestinalObstruction(emergencyRoomSevereCapacityInfo.get().getInfantIntestinalObstruction());
+            eSevereResponseDto.setEmergencyGastrointestinalEndoscopy(emergencyRoomSevereCapacityInfo.get().getEmergencyGastrointestinalEndoscopy());
+            eSevereResponseDto.setEmergencyGastrointestinalEndoscopyForChildren(emergencyRoomSevereCapacityInfo.get().getEmergencyGastrointestinalEndoscopyForChildren());
+            eSevereResponseDto.setEmergencyBronchoscopy(emergencyRoomSevereCapacityInfo.get().getEmergencyBronchoscopy());
+            eSevereResponseDto.setEmergencyBronchoscopyForChildren(emergencyRoomSevereCapacityInfo.get().getEmergencyBronchoscopyForChildren());
+            eSevereResponseDto.setLowBirthWeightInfant(emergencyRoomSevereCapacityInfo.get().getLowBirthWeightInfant());
+            eSevereResponseDto.setObstetricDelivery(emergencyRoomSevereCapacityInfo.get().getObstetricDelivery());
+            eSevereResponseDto.setObstetricSurgery(emergencyRoomSevereCapacityInfo.get().getObstetricSurgery());
+            eSevereResponseDto.setEmergencyGynecologicalSurgery(emergencyRoomSevereCapacityInfo.get().getEmergencyGynecologicalSurgery());
+            eSevereResponseDto.setSevereBurns(emergencyRoomSevereCapacityInfo.get().getSevereBurns());
+            eSevereResponseDto.setLimbReattachmentExtremities(emergencyRoomSevereCapacityInfo.get().getLimbReattachmentExtremities());
+            eSevereResponseDto.setLimbReattachmentOther(emergencyRoomSevereCapacityInfo.get().getLimbReattachmentOther());
+            eSevereResponseDto.setEmergencyDialysisHD(emergencyRoomSevereCapacityInfo.get().getEmergencyDialysisHD());
+            eSevereResponseDto.setEmergencyDialysisCRRT(emergencyRoomSevereCapacityInfo.get().getEmergencyDialysisCRRT());
+            eSevereResponseDto.setPsychiatry(emergencyRoomSevereCapacityInfo.get().getPsychiatry());
+            eSevereResponseDto.setOphthalmicSurgery(emergencyRoomSevereCapacityInfo.get().getOphthalmicSurgery());
+            eSevereResponseDto.setRadiologyVascularIntervention(emergencyRoomSevereCapacityInfo.get().getRadiologyVascularIntervention());
+            eSevereResponseDto.setRadiologyVascularInterventionForChildren(emergencyRoomSevereCapacityInfo.get().getRadiologyVascularInterventionForChildren());
+            eSevereResponseDto.setInfantIntestinalAge(emergencyRoomSevereCapacityInfo.get().getInfantIntestinalAge());
+            eSevereResponseDto.setGastrointestinalEndoscopyAge(emergencyRoomSevereCapacityInfo.get().getGastrointestinalEndoscopyAge());
+            eSevereResponseDto.setBronchoscopyAge(emergencyRoomSevereCapacityInfo.get().getBronchoscopyAge());
+            eSevereResponseDto.setLowBirthWeightAge(emergencyRoomSevereCapacityInfo.get().getLowBirthWeightAge());
+            eSevereResponseDto.setRadiologyAge(emergencyRoomSevereCapacityInfo.get().getRadiologyAge());
+            eSevereResponseDto.setUpdateTime(emergencyRoomSevereCapacityInfo.get().getUpdateTime());
 
+
+            return eSevereResponseDto;
+        }
+        else{
+            log.error("[getEmergencySevere] emergencyRoomSevereCapacityInfo is not present");
+            throw new RuntimeException("emergencyRoomSevereCapacityInfo is not present");
+        }
+    }
+
+    public List<MessageResponseDto> getEmergencyMessage(String dutyId){
+        log.info("[getEmergencyMessage] input dutyId:{}",dutyId);
+        List<MessageResponseDto> messageResponseDtoList=emergencyMessageRepository.findByEmergencyMessageDutyId(dutyId)
+                        .stream().map(MessageResponseDto::new).toList();
+        return messageResponseDtoList;
     }
 
     /*
-
     @Override
     public ProductResponseDto getProduct(Long number){
         LOGGER.info("[getProduct] input number:{}",number);
